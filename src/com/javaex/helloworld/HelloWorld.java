001a1a4658ed22dbf5b758d94f3791e4ca756003
hi <hi@DESKTOP-A3OJ2BV>
2020-05-07 10:47:45 +0900

    first commit

---
+++ src/com/javaex/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ 작성자 : 황민영
+ 작성일 : 20. 05. 01
+ 설  명 : Hello World 출력하기
+ */
+package com.javaex.helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		// Hello World 출력
+		System.out.println("Hello world!");
+	}
+}
